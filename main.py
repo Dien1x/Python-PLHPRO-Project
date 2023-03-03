da551b522514c7e891941d49782acbbab5aa2adb
Dienix <124458790+Dien1x@users.noreply.github.com>
2023-03-03 12:13:37 +0200

    Enemy
    Enemy can now Randomize his ships and do random artillery Fire

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from tkinter import *
+import random
+import time
+
+# Αρχικοποίηση διαφορετικής αλληλουχίας για να έχουμε κάθε φορά διαφορετικά αποτελέσματα
+random.seed(time.time())
 
 PLAYER_NAME = ""
 player_table = [[] for i in range(11)]
@@ -7,6 +12,25 @@ battleships = {"aircraft_carrier": [0, 5],
                "battleship": [0, 4],
                "cruiser": [0, 3],
                "destroyer": [0, 2]}
+# Μία λίστα η οποία περιέχει όλες τις δυνατές επιλογές που μπορεί να κάνει ο υπολογιστής
+# κατά τη διάρκεια της βολής του
+CPU_random_choice = []
+for y in range(11):
+    for x in range(10):
+        CPU_random_choice.append([x, y])
+# Μία λίστα με συνένωση συντεταγμένων σε περίπτωση επιτυχής βολής
+CPU_random_choice_if_hit = ["",[]]
+HIT = False
+# Μία λίστα με τις συντεταγμένες των πλοίων του εχθρού
+enemy_battleships = []
+# Λίστα με τα κουμπιά που αλλάζουν ανάλογα με τις επιλογές του παίχτη
+enemy_right_wrong_buttons = [[], []]
+# Λίστα με τις ταμπέλες που αλλάζουν ανάλογα με τις επιλογές του Υπολογιστή
+friendly_right_wrong_labels = [[], []]
+
+# Score counter
+PLAYER_SCORE = 0
+PC_SCORE = 0
 
 
 class Ship:
@@ -94,7 +118,7 @@ def create_ship(ship):
                 """Ανάλογα με τη δοθείσα κατεύθυνση συμπληρώνει τις συντεταγμένες του αντικειμένου."""
 
                 if direction == "up":
-                    for size in range(battleships[ship][2].size):
+                    for size in range(1, battleships[ship][2].size):
                         battleships[ship][2].coordinates.append([x, y-size])
                     for size in range(battleships[ship][2].size):
                         if x-1 >= 0:
@@ -108,7 +132,7 @@ def create_ship(ship):
                         if y-battleships[ship][2].size and 0 <= x - 1 + i < 10:
                             battleships[ship][2].adjacent_coordinates.append([x-1+i, y-battleships[ship][2].size])
                 elif direction == "down":
-                    for size in range(battleships[ship][2].size):
+                    for size in range(1, battleships[ship][2].size):
                         battleships[ship][2].coordinates.append([x, y+size])
                     for size in range(battleships[ship][2].size):
                         if x - 1 >= 0:
@@ -122,7 +146,7 @@ def create_ship(ship):
                         if y+battleships[ship][2].size < 11 and 0 <= x - 1 + i < 10:
                             battleships[ship][2].adjacent_coordinates.append([x-1+i, y+battleships[ship][2].size])
                 elif direction == "right":
-                    for size in range(battleships[ship][2].size):
+                    for size in range(1, battleships[ship][2].size):
                         battleships[ship][2].coordinates.append([x+size, y])
                     for size in range(battleships[ship][2].size):
                         if y-1 >= 0:
@@ -136,7 +160,7 @@ def create_ship(ship):
                         if x+battleships[ship][2].size < 10 and 0 <= y-1+i < 11:
                             battleships[ship][2].adjacent_coordinates.append([x+battleships[ship][2].size, y-1+i])
                 else:
-                    for size in range(battleships[ship][2].size):
+                    for size in range(1, battleships[ship][2].size):
                         battleships[ship][2].coordinates.append([x-size, y])
                     for size in range(battleships[ship][2].size):
                         if y-1 >= 0:
@@ -150,7 +174,7 @@ def create_ship(ship):
                         if x-battleships[ship][2].size >= 0 and 0 <= y-1+i < 11:
                             battleships[ship][2].adjacent_coordinates.append([x-battleships[ship][2].size, y-1+i])
 
-                new_game(window, False)
+                new_game(window, False, True)
 
             # Αρχικοποίηση συντεταγμένων κεφαλής αντικειμένου.
             battleships[ship][2].coordinates.append([x, y])
@@ -249,7 +273,7 @@ def create_ship(ship):
         if battleships[someship][0] == 1:
             battleships[someship][2].create_ship_buttons()
             battleships[someship][2].create_ship_adjacent_buttons()
-            for i in range(len(battleships[someship][2].buttons)):
+            for i in range(len(battleships[someship][2].buttons)-1):
                 battleships[someship][2].buttons[i].grid(row=battleships[someship][2].coordinates[i][1],
                                                          column=battleships[someship][2].coordinates[i][0])
             for i in range(len(battleships[someship][2].adjacent_coordinates)):
@@ -311,7 +335,7 @@ def del_ship(ship):
 
     del battleships[ship][2]
     battleships[ship][0] = 0
-    new_game(window, False)
+    new_game(window, False, True)
 
 
 def remove_ship():
@@ -347,6 +371,66 @@ def remove_ship():
     center_window(window)
 
 
+def start_game():
+    """
+    Συνάρτηση έναρξης παιχνιδιού.
+
+    Με τη συνάρτηση αυτή ο Υπολογιστής τοποθετεί τα πλοία του και ξεκινάει το παιχνίδι
+    """
+
+    global CPU_random_choice
+
+    ship = 2
+    while ship != 6:
+        # Συντεταγμένες για να επιλέξει ο υπολογιστής κατεύθυνση
+        x = [-1, 1]
+        y = [-1, 1]
+        # Διαλέξει ένα τυχαίο σημείο στο πλαίσιο
+        coords = random.choice(CPU_random_choice)
+        CPU_random_choice.remove(coords)
+        direction = random.choice(["x", "y"])
+        # Περιορισμοί σε περίπτωση σύγκρουσης
+        if direction == "x":
+            y = [0]
+            if coords[0]-ship+1 < 0:
+                x.remove(-1)
+            if coords[0]+ship > 9:
+                x.remove(1)
+            if not x:
+                break
+        if direction == "y":
+            x = [0]
+            if coords[1]-ship+1 < 0:
+                y.remove(-1)
+            if coords[1]+ship > 10:
+                y.remove(1)
+            if not y:
+                break
+        enemy_ship = [coords]
+        random_x = random.choice(x)
+        random_y = random.choice(y)
+        for i in range(ship-1):
+            enemy_ship += [[coords[0]+(1+i)*random_x, coords[1]+(1+i)*random_y]]
+        # Περιορισμοί σε περίπτωση επαφής με άλλο πλοίο
+        if enemy_battleships:
+            for enemy_battleship_coords in enemy_battleships:
+                for enemy_ship_coords in enemy_ship:
+                    if enemy_ship_coords == enemy_battleship_coords:
+                        break
+        for i in enemy_ship:
+            enemy_battleships.append(i)
+
+        ship += 1
+
+    # Ανακατασκευή της λίστας CPU_random_choice
+    CPU_random_choice = []
+    for y in range(11):
+        for x in range(10):
+            CPU_random_choice.append([x, y])
+
+    new_game(window, False, False)
+
+
 def center_window(window):
     """
     Μία Συνάρτηση κεντραρίσματος.
@@ -361,7 +445,7 @@ def center_window(window):
     window.geometry(f"{window.winfo_width()}x{window.winfo_height()}+{x}+{y}")
 
 
-def new_game(window, entry_box):
+def new_game(window, entry_box, start_frame):
     """
     Έναρξη νέου παιχνιδιού.
 
@@ -373,18 +457,76 @@ def new_game(window, entry_box):
     player_table = [[] for i in range(11)]
     enemy_table = [[] for i in range(11)]
 
-    global PLAYER_NAME
+    global PLAYER_NAME, PLAYER_SCORE, PC_SCORE
     if entry_box:
         PLAYER_NAME = ""
         PLAYER_NAME = player_name_box.get()
 
-    ### Συνάρτηση η οποία δέχεται τη θέση του κάθε κουμπιού και επιστρέφει διαφορετική εντολή για κάθε κουμπί
-    ##def enemy_buttons(x, y):
-        ### Συνάρτηση διαφορετική για κάθε κουμπί
-        ##def enemy():
-            ##enemy_table[y][x].config(state=DISABLED, bg="red")
-            ##print(x, y)
-        ##return enemy
+    # Συνάρτηση η οποία δέχεται τη θέση του κάθε κουμπιού και επιστρέφει διαφορετική εντολή για κάθε κουμπί
+    def enemy_buttons(x, y):
+        # Συνάρτηση διαφορετική για κάθε κουμπί
+        def enemy():
+            global PLAYER_SCORE
+            global PC_SCORE
+            global HIT
+            global CPU_random_choice_if_hit
+
+            label_1 = Label(window, text=f"Κάνατε πυρά στο σημείο ({x-1}, {y-1})")
+            label_2 = Label(window, text="Παρακαλώ περιμένετε")
+            if [x, y] in enemy_battleships:
+                enemy_right_wrong_buttons[0].append([x, y])
+                PLAYER_SCORE += 1
+                label_2.config(text="Πετύχατε κάποιο εχθρικό πλοίο!!")
+            else:
+                enemy_right_wrong_buttons[1].append([x, y])
+                label_2.config(text="Τα πυρά σας ήταν εκτός στόχου!!")
+
+            if not HIT:
+                enemy_choice = random.choice(CPU_random_choice)
+            else:
+                if CPU_random_choice_if_hit[1]:
+                    enemy_choice = random.choice(CPU_random_choice_if_hit[1])
+                else:
+                    HIT = False
+                    del battleships[CPU_random_choice_if_hit[0]]
+                    enemy_choice = random.choice(CPU_random_choice)
+
+            if not HIT:
+                for ship in battleships.keys():
+                    for ship_coords in battleships[ship][2].coordinates:
+                        if enemy_choice == ship_coords:
+                            friendly_right_wrong_labels[0].append(enemy_choice)
+                            CPU_random_choice_if_hit[1] = battleships[ship][2].coordinates + battleships[ship][2].adjacent_coordinates
+                            i = 0
+                            while i < len(CPU_random_choice_if_hit[1]):
+                                if CPU_random_choice_if_hit[1][i] not in CPU_random_choice:
+                                    CPU_random_choice_if_hit[1].remove(CPU_random_choice_if_hit[1][i])
+                                else:
+                                    i += 1
+                            CPU_random_choice_if_hit[0] = ship
+                            CPU_random_choice_if_hit[1].remove(enemy_choice)
+                            HIT = True
+                            CPU_random_choice.remove(enemy_choice)
+                            PC_SCORE += 1
+                            break
+            else:
+                if enemy_choice in battleships[CPU_random_choice_if_hit[0]][2].coordinates:
+                    friendly_right_wrong_labels[0].append(enemy_choice)
+                    CPU_random_choice.remove(enemy_choice)
+                    CPU_random_choice_if_hit[1].remove(enemy_choice)
+                    PC_SCORE += 1
+                else:
+                    friendly_right_wrong_labels[1].append(enemy_choice)
+                    CPU_random_choice.remove(enemy_choice)
+                    CPU_random_choice_if_hit[1].remove(enemy_choice)
+
+            if not HIT:
+                CPU_random_choice.remove(enemy_choice)
+                friendly_right_wrong_labels[1].append(enemy_choice)
+
+            new_game(window, False, False)
+
+        return enemy
 
     # Αφαιρεί τη δυνατότητα χρήσης του Enter
     window.unbind("<Return>")
@@ -398,23 +540,18 @@ def new_game(window, entry_box):
                           text=f"{PLAYER_NAME}",
                           font=("Comic Sans MS", 20))
     active_player.grid(row=0, column=1, columnspan=2)
-    # Ετικέτα που εμφανίζει στο παράθυρο ένα χρονόμετρο για αντίστροφη μέτρηση
-    timer = Label(window,
-                  text="12:00",
-                  font=("Comic Sans MS", 15))
-    timer.grid(row=1, column=1)
     # Ετικέτα που εμφανίζει στο παράθυρο το Score
     score = Label(window,
-                  text=f"{PLAYER_NAME}: 1\tΗ/Υ: 0",
+                  text=f"{PLAYER_NAME}: {PLAYER_SCORE}\tCPU: {PC_SCORE}",
                   font=("Comic Sans MS", 15))
-    score.grid(row=1, column=2)
+    score.grid(row=1, column=1, columnspan=2)
 
     # Μία δομή για αποθήκευση όλων των ετικετών που απαρτίζουν το Ταμπλό του Παίχτη
     frame_player = Frame(window)
     # Μία δομή για αποθήκευση όλων των κουμπιών που απαρτίζουν το Ταμπλό του Εχθρού
     frame_enemy = Frame(window, padx=15, pady=15)
     # Μία δομή για αποθήκευση των κουμπιών για τοποθέτηση πλοίων
-    frame_ship_input = Frame(window, padx=15, pady=15)
+    frame_ship_input = Frame(window, padx=15, pady=15, name="frame_ship_input")
     # Μία δομή για αποθήκευση των κουμπιών "Νέο Παιχνίδι" και "Έξοδος"
     frame_start_stop = Frame(window)
 
@@ -435,7 +572,8 @@ def new_game(window, entry_box):
                          font=("Comic Sans MS", 15), state=DISABLED)
     # Κουμπί για Έναρξη Παιχνιδιού
     start = Button(frame_ship_input, text="Έναρξη παιχνιδιού",
-                   font=("Comic Sans MS", 15))
+                   font=("Comic Sans MS", 15),
+                   command=start_game)
     start.grid(row=1, column=0, columnspan=2, padx=10)
     ship_add.grid(row=0, column=0, padx=10)
     ship_remove.grid(row=0, column=1, padx=10)
@@ -443,17 +581,21 @@ def new_game(window, entry_box):
     # Στοίχιση δομών στο παράθυρο
     frame_player.grid(row=3, column=1, columnspan=2)
     frame_enemy.grid(row=0, column=0, rowspan=5)
-    frame_ship_input.grid(row=6, column=0, rowspan=2)
+    if start_frame:
+        frame_ship_input.grid(row=6, column=0, rowspan=2)
     frame_start_stop.grid(row=4, column=1, columnspan=2)
 
     # Δημιουργία και εισαγωγή των κουμπιών στο ταμπλό του εχθρού
     for y in range(11):
         for x in range(10):
-            ###enemy = enemy_buttons(x, y)  # Ορίζεται στην αρχή της συνάρτησης
+            enemy = enemy_buttons(x, y)  # Ορίζεται στην αρχή της συνάρτησης
             button = Button(frame_enemy, text=f"{x+1}, {y+1}",
-                            width=9, height=4, state=DISABLED)
-            enemy_table[x].append(button)
-            enemy_table[x][y].grid(row=y, column=x)
+                            width=9, height=4, state=DISABLED, command=enemy)
+            # Τα κουμπιά θα είναι ενεργά μόνο όταν ξεκινήσει το παιχνίδι
+            if not start_frame:
+                button.config(state=NORMAL)
+            enemy_table[y].append(button)
+            enemy_table[y][x].grid(row=y, column=x)
 
     # Δημιουργία και εισαγωγή των ετικετών στο ταμπλό του Παίχτη
     for y in range(11):
@@ -473,7 +615,7 @@ def new_game(window, entry_box):
     for ship in battleships.keys():
         if battleships[ship][0] == 1:
             battleships[ship][2].create_ship_labels(frame_player)
-            for i in range(len(battleships[ship][2].labels)):
+            for i in range(len(battleships[ship][2].labels)-1):
                 battleships[ship][2].labels[i].grid(row=battleships[ship][2].coordinates[i][1],
                                                     column=battleships[ship][2].coordinates[i][0])
 
@@ -488,6 +630,24 @@ def new_game(window, entry_box):
         if battleships[ship][0] == 1:
             ship_remove.config(state=NORMAL)
             break
+
+    if enemy_right_wrong_buttons[0]:
+        for button in enemy_right_wrong_buttons[0]:
+            enemy_table[button[1]][button[0]].config(state=DISABLED, bg="red")
+
+    if enemy_right_wrong_buttons[1]:
+        for button in enemy_right_wrong_buttons[1]:
+            enemy_table[button[1]][button[0]].config(state=DISABLED, bg="white")
+
+    if friendly_right_wrong_labels[0]:
+        for label_coords in friendly_right_wrong_labels[0]:
+            Label(frame_player, text=f"{label_coords[0] + 1}, {label_coords[1] + 1}",
+                  width=6, height=3, bg="red").grid(row=label_coords[1], column=label_coords[0])
+
+    if friendly_right_wrong_labels[1]:
+        for label_coords in friendly_right_wrong_labels[1]:
+            Label(frame_player, text=f"{label_coords[0] + 1}, {label_coords[1] + 1}",
+                  width=6, height=3, bg="white").grid(row=label_coords[1], column=label_coords[0])
 
     center_window(window)
 
@@ -509,7 +669,7 @@ player_name_box = Entry(window,
 player_name_button = Button(window,
                             text="Νέο Παιχνίδι",
                             font=("Comic Sans MS", 14, "bold"),
-                            command=lambda: new_game(window, True))
+                            command=lambda: new_game(window, True, True))
 
 player_name_label.pack()
 player_name_box.pack()
@@ -517,7 +677,7 @@ player_name_button.pack()
 center_window(window)
 
 # Προσθέτει τη δυνατότητα καταχώρησης ονόματος με το Enter
-window.bind("<Return>", lambda event: new_game(window, True))
+window.bind("<Return>", lambda event: new_game(window, True, True))
 
 
 window.mainloop()
